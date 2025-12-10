6fe54bc465e6359b65fd95f66453f38b85d7d7dd
ReplitUser329 <replituser329@gmail.com>
2025-12-10 01:16:42 -0800

    Create teams.js

---
+++ src/app/src/app/src/app/src/components/src/data/src/app/standings/src/app/teams/src/app/teams/[id]/src/data/teams.js
@@ -0,0 +1,7 @@
+const teams = [
+  { id: "lakers", name: "Los Angeles Lakers", city: "Los Angeles", conference: "West", division: "Pacific", logo: "lakers.png" },
+  { id: "celtics", name: "Boston Celtics", city: "Boston", conference: "East", division: "Atlantic", logo: "celtics.png" },
+  { id: "warriors", name: "Golden State Warriors", city: "San Francisco", conference: "West", division: "Pacific", logo: "warriors.png" },
+];
+
+export default teams;
